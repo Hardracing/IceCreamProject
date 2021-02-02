e6e3db8581a70b5add4183822c21f474b698fb01
LidiaStrelets <0630610171li@gmail.com>
2021-02-02 12:04:03 +0200

    Updates

--- mobile-menu.02d75e0a.js
+++ mobile-menu.02d75e0a.js
@@ -117,83 +117,19 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sass/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"/Users/lidia_strelets/Documents/GitHub/IceCreamProject/src/images/making-img/mobile/milk-background@1x.png":[["milk-background@1x.bb2a46ae.png","images/making-img/mobile/milk-background@1x.png"],"images/making-img/mobile/milk-background@1x.png"],"/Users/lidia_strelets/Documents/GitHub/IceCreamProject/src/images/making-img/mobile/milk-background@2x.png":[["milk-background@2x.e2d8f3ee.png","images/making-img/mobile/milk-background@2x.png"],"images/making-img/mobile/milk-background@2x.png"],"/Users/lidia_strelets/Documents/GitHub/IceCreamProject/src/images/making-img/desktop/milk-background@1x.png":[["milk-background@1x.5ceda0c7.png","images/making-img/desktop/milk-background@1x.png"],"images/making-img/desktop/milk-background@1x.png"],"/Users/lidia_strelets/Documents/GitHub/IceCreamProject/src/images/making-img/desktop/milk-background@2x.png":[["milk-background@2x.72e39ce0.png","images/making-img/desktop/milk-background@2x.png"],"images/making-img/desktop/milk-background@2x.png"],"/Users/lidia_strelets/Documents/GitHub/IceCreamProject/src/images/making-buttton-arrow.svg":[["making-buttton-arrow.2d668fdd.svg","images/making-buttton-arrow.svg"],"images/making-buttton-arrow.svg"],"/Users/lidia_strelets/Documents/GitHub/IceCreamProject/src/images/bucket-milk.svg":[["bucket-milk.00dc0a46.svg","images/bucket-milk.svg"],"images/bucket-milk.svg"],"/Users/lidia_strelets/Documents/GitHub/IceCreamProject/src/images/bucket-apple.svg":[["bucket-apple.b22589eb.svg","images/bucket-apple.svg"],"images/bucket-apple.svg"],"/Users/lidia_strelets/Documents/GitHub/IceCreamProject/src/images/sweets.svg":[["sweets.ae05f5b3.svg","images/sweets.svg"],"images/sweets.svg"],"./../images/reviews/line1.svg":[["line1.a521c0fc.svg","images/reviews/line1.svg"],"images/reviews/line1.svg"],"./../images/reviews/icons/quotes1.svg":[["quotes1.6dac1593.svg","images/reviews/icons/quotes1.svg"],"images/reviews/icons/quotes1.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./sass/main.scss");
-},{"./sass/main.scss":"sass/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"js/mobile-menu.js":[function(require,module,exports) {
+(function () {
+  var menuBtnRef = document.querySelector("[data-menu-button]");
+  var mobileMenuRef = document.querySelector("[data-menu]");
+  menuBtnRef.addEventListener('click', function () {
+    var expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    menuBtnRef.classList.toggle("is-opened");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    mobileMenuRef.classList.toggle("is-opened");
+    document.body.classList.toggle("modal-open");
+  });
+})();
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -397,5 +333,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/mobile-menu.js"], null)
+//# sourceMappingURL=/mobile-menu.02d75e0a.js.map
